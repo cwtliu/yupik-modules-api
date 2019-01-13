9d910f21dae875b07f7fd754272e600d5c0fdf40
Temigo <temigo@gmx.com>
2019-01-12 16:06:46 -0800

    test

--- api.py
+++ api.py
@@ -222,7 +222,8 @@ class Audio(Resource):
         filename = url_for('static', filename='exercise1/'+word+'.mp3')
         print(filename)
         mp3 = urllib.urlopen(filename).read()
-        a = AudioSegment.from_mp3(BytesIO(mp3))
+        x = BytesIO(mp3)
+        a = AudioSegment.from_mp3(x)
         a.export('/tmp/test.mp3', format='mp3')
         return send_file('/tmp/test.mp3', mimetype='audio/mp3')
 
